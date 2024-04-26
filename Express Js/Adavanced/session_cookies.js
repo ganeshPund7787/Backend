fe7908a5e9efb952b7ec829cc13e7d6906dae3c4
Ganesh Pund <ganupund7787@gmail.com>
2024-04-26 04:11:41 -0700

    revision

--- Express Js/Adavanced/session_cookies.js
+++ Express Js/Adavanced/session_cookies.js
@@ -6,7 +6,7 @@ import cookie from "cookie-parser"
 const app = express();
 const router = express.Router();
 
-app.use(BodyParser.urlencoded({extended: false}));
+app.use(BodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(session({
     resave: false,
@@ -15,17 +15,14 @@ app.use(session({
 }))
 app.use(cookie());
 
-
-
 app.get("/", (req, res) => {
     req.session.SessionName = true;
     console.log(session());
     res.status(200).json({
         success: true,
-        session 
+        session
     });
 })
-
 
 app.get("/check", (req, res) => {
     if (req.session.SessionName != true) {
@@ -34,7 +31,7 @@ app.get("/check", (req, res) => {
             success: true,
             session
         });
-    } else { 
+    } else {
         res.status(400).json({
             success: false,
             session
@@ -44,7 +41,7 @@ app.get("/check", (req, res) => {
 
 
 
-app.get("/delete", async(req, res) => {
+app.get("/delete", async (req, res) => {
     if (req.session.SessionName == true) {
         const sess = await req.sessionID();
         console.log(sess);
@@ -52,16 +49,13 @@ app.get("/delete", async(req, res) => {
             success: true,
             session
         });
-    } else { 
+    } else {
         res.status(400).json({
             success: false,
             session
         });
     }
 })
-
-
-
 
 
 
@@ -71,9 +65,9 @@ app.get("/", (req, res) => {
     res.send("hey");
 });
 
-app.get("/:username", (req, res) => {
-    res.json(`Hellow ${req.params.username} welcome to our website`);
-});
+// app.get("/:username", (req, res) => {
+//     res.json(`Hellow ${req.params.username} welcome to our website`);
+// });
 
 
 app.get("/", (req, res) => {
@@ -96,7 +90,7 @@ app.get("/deletecookie", (req, res) => {
 
 app.get("/checkban", (req, res) => {
     console.log("Check Kiya consle dekho");
-    if(req.session.SessionName == true) res.send("You are baned"); 
+    if (req.session.SessionName == true) res.send("You are baned");
     else res.send(req.session);
 });
 
@@ -106,7 +100,11 @@ app.get("/removeban", (req, res) => {
         else res.send("You Are Remove Ban");
     })
 })
-app.listen(4000);
+
+
+app.listen(4000, () => {
+    console.log('server is workng on http://localhost:4000');
+});
 
 
 
